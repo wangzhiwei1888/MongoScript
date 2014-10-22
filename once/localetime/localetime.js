aea39a32d9c45017393fe454ddba45b867bcd216
stevennuo <3er.com.cn@gmail.com>
2014-10-22 12:30:04 +0800

    only update blank localetime

--- once/localetime/localetime.js
+++ once/localetime/localetime.js
@@ -1,7 +1,7 @@
 // 因为运营需要很多以天计算的的数据分析请求，而Mongo中确实用UTC来存储时间的，且比如$dayofweek等内置时间运算符都是基于utc时间，无法基于本地时间。
 // 故存储时存储两个时间
 
-db.tracks.find().forEach(function (doc) {
+db.tracks.find({localetime:{$exists: false}}).forEach(function (doc) {
 //    print(doc.timestamp)
 //    print(new Date(doc.timestamp.getTime() + 8 * 60 * 60 * 1000 ));
 
