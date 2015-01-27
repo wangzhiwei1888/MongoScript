d827eb4d3fa7db0baec0d625114357daeed0eea8
stevennuo <3er.com.cn@gmail.com>
2015-01-27 18:26:01 +0800

    course script update

---
+++ transfer/course2.js
@@ -0,0 +1,621 @@
+var chapters = [{"chapter":"有理数","topics":[
+{
+    "name":"负数的定义",
+    "elementary":["53c64bc02e2c21e836470974"],
+    "advanced":[],
+    "challenge":["53c5dd360dfa1851145f884c"]
+},{
+    "name":"有理数的概念和分类",
+    "elementary":["53c64c082e2c21e836470975"],
+    "advanced":[],
+    "challenge":["53c5dda30dfa1851145f884d"]
+ },{
+    "name":"数轴",
+    "elementary":["53c64c5f2e2c21e836470976"],
+    "advanced":[],
+    "challenge":["53c5ddd90dfa1851145f884e"]
+ },{
+    "name":"相反数",
+    "elementary":["53c64d1e2e2c21e836470977"],
+    "advanced":["53d86ec3af3249f2747d7000"],
+    "challenge":["53c60dac0dfa1851145f88f8"]
+ },{
+    "name":"绝对值",
+    "elementary":["53c64d6a2e2c21e836470978"],
+    "advanced":[],
+    "challenge":["53c5de2b0dfa1851145f8850"]
+ },{
+    "name":"有理数四则运算介绍",
+    "elementary":["53c79a572e2c21e836470e28"],
+    "advanced":[],
+    "challenge":[]
+ },{
+    "name":"有理数的加法",
+    "elementary":["53c64e972e2c21e83647097a","53c64ee82e2c21e83647097b"],
+    "advanced":[],
+    "challenge":["53c649652e2c21e83647096c"]
+ },{
+    "name":"有理数的减法",
+    "elementary":["53c64f5f2e2c21e83647097c"],
+    "advanced":[],
+    "challenge":["53c649bb2e2c21e83647096d"]
+ },{
+    "name":"有理数的加减混合运算",
+    "elementary":[],
+    "advanced":["53c72de52e2c21e83647098d"],
+    "challenge":["53ec51011cb4aae95a4b31d2"]
+ },{
+    "name":"有理数的乘法",
+    "elementary":["53c64fc62e2c21e83647097d","53c72a382e2c21e836470982"],
+    "advanced":["53d86f9baf3249f2747d7009"],
+    "challenge":["53c64a712e2c21e83647096f"]
+ },{
+    "name":"有理数的除法",
+    "elementary":["53c72aa02e2c21e836470983"],
+    "advanced":[],
+    "challenge":["53c64aae2e2c21e836470970"]
+ },{
+    "name":"有理数的四则混合运算",
+    "elementary":[],
+    "advanced":["53d87047af3249f2747d7015"],
+    "challenge":[]
+ },{
+    "name":"有理数的乘方",
+    "elementary":["53c72b142e2c21e836470984","53d5c6f6af3249f2747d49b6"],
+    "advanced":[],
+    "challenge":["53c64ae42e2c21e836470971"]
+ },{
+    "name":"有理数的四则及乘方混合运算",
+    "elementary":[],
+    "advanced":["53d870d5af3249f2747d702b"],
+    "challenge":["53c733a32e2c21e8364709a4"]
+ },{
+    "name":"科学记数法",
+    "elementary":["53c72b562e2c21e836470985"],
+    "advanced":[],
+    "challenge":["53c64b222e2c21e836470972"]
+ }
+]},
+{"chapter":"整式的加减","topics":[
+{
+    "name":"符号语言和书写规范",
+    "elementary":["53e877c6fba13fad32b92911"],
+    "advanced":["53e87831fba13fad32b92912"],
+    "challenge":["53e1cac56a3401fd615be2ff"]
+},{
+    "name":"单项式",
+    "elementary":["53e990e3fba13fad32b92a91"],
+    "advanced":["53e990f7fba13fad32b92a92"],
+    "challenge":["53eb3b7c1cb4aae95a4b2fda"]
+ },{
+    "name":"单项式的系数与次数",
+    "elementary":["53e99112fba13fad32b92a93"],
+    "advanced":["53e9912efba13fad32b92a94"],
+    "challenge":["53f1a56880202a461da7a250"]
+ },{
+    "name":"多项式",
+    "elementary":["53e9915dfba13fad32b92a95"],
+    "advanced":["53e9918dfba13fad32b92a97"],
+    "challenge":["53f197cc80202a461da7a1a9"]
+ },{
+    "name":"升幂降幂",
+    "elementary":[],
+    "advanced":["53e99174fba13fad32b92a96"],
+    "challenge":["53f576c16dc0068a6f107fe6"]
+ },{
+    "name":"同类项",
+    "elementary":["53e991bcfba13fad32b92a98"],
+    "advanced":["53e992aefba13fad32b92a9b"],
+    "challenge":["53e98c9afba13fad32b92a42"]
+ },{
+    "name":"合并同类项",
+    "elementary":["53e991dffba13fad32b92a99"],
+    "advanced":["540e74f8bb122f407143032d"],
+    "challenge":["53eb0c221cb4aae95a4b2e86"]
+ },{
+    "name":"去括号",
+    "elementary":["53e9ad6bfba13fad32b92b47"],
+    "advanced":["53e9ad9bfba13fad32b92b48"],
+    "challenge":["53ec2a791cb4aae95a4b30a5"]
+ },{
+    "name":"含字母系数整式的化简",
+    "elementary":[],
+    "advanced":["53e9ae48fba13fad32b92b4e"],
+    "challenge":[]
+ },{
+    "name":"先化简再求值",
+    "elementary":[],
+    "advanced":["53e9ae60fba13fad32b92b4f"],
+    "challenge":["53fbe9081923773218f3ec48"]
+ },{
+    "name":"整体代入",
+    "elementary":[],
+    "advanced":["53eb34de1cb4aae95a4b2f84"],
+    "challenge":["53f6e753f347b6a575306391"]
+ },{
+    "name":"找规律",
+    "elementary":["541934a0a5f980790573e790"],
+    "advanced":[],
+    "challenge":["541934a7a5f980790573e791"]
+ }
+]},
+{"chapter":"一元一次方程","topics":[
+{
+    "name":"方程与一元一次方程",
+    "elementary":["5433aa8fb97968317c5af31b"],
+    "advanced":["5433aa93b97968317c5af31d","5433aa95b70f2e237c5e5e84"],
+    "challenge":["5433aa90b97968317c5af31c"]
+},{
+    "name":"找等量关系列方程",
+    "elementary":[],
+    "advanced":["5433aa97b70f2e237c5e5e86"],
+    "challenge":["5433aa9ab97968317c5af321"]
+ },{
+    "name":"等式的性质",
+    "elementary":["5433ba39b97968317c5b5a8b"],
+    "advanced":[],
+    "challenge":[]
+ },{
+    "name":"合并同类项、移项解方程",
+    "elementary":["5433ba3fb70f2e237c5ec0cf"],
+    "advanced":[],
+    "challenge":["5433ba41b70f2e237c5ec0d0"]
+ },{
+    "name":"去括号、去分母解方程",
+    "elementary":["5433ba45b97968317c5b5a91"],
+    "advanced":["5433ba57b97968317c5b5bfd"],
+    "challenge":["5433ba53b70f2e237c5ec0da"]
+ },{
+    "name":"方程的解",
+    "elementary":[],
+    "advanced":["5433ba5fb97968317c5b5c32"],
+    "challenge":["5433ba5cb97968317c5b5c31"]
+ },{
+    "name":"带你围观应用题上",
+    "elementary":["5433bc10b97968317c5b7135"],
+    "advanced":["543b8ec73bfd788a03c64246","543b8ece3bfd788a03c64432"],
+    "challenge":["5433bc14b70f2e237c5ed1be"]
+ },{
+    "name":"带你围观应用题下",
+    "elementary":["543b8ec13bfd788a03c640b6"],
+    "advanced":["543b8ed44d4d5b8803f1f1c5"],
+    "challenge":["543b8f914d4d5b8803f1f805"]
+ }
+]},
+{"chapter":"实数","topics":[
+{
+    "name":"平方根",
+    "elementary":["546480fc137e48c722de8ff2"],
+    "advanced":[],
+    "challenge":["54648141e0d39bdc22513696"]
+},{
+    "name":"算数平方根",
+    "elementary":["54648103e0d39bdc2251368f"],
+    "advanced":["5464813dc4d33abb22bcfc55","5464816e6dc71d4122c8b1b4"],
+    "challenge":["546570e4c4d33abb22bf506b"]
+ },{
+    "name":"立方根",
+    "elementary":["546480f4e0d39bdc2251368d"],
+    "advanced":["54648101e0d39bdc2251368e"],
+    "challenge":["546570e7c4d33abb22bf506c"]
+ },{
+    "name":"根号2也有组织",
+    "elementary":["546481a9c4d33abb22bcfcca"],
+    "advanced":[],
+    "challenge":["546481a5a727825d224b2f5b"]
+ },{
+    "name":"实数的相反数绝对值和简单运算",
+    "elementary":["546481b61603e4db22595c72"],
+    "advanced":["546481a26dc71d4122c8b1b8","546481afa727825d224b2f5d"],
+    "challenge":["54657436e0d39bdc2254b877"]
+ }
+]},
+{"chapter":"三角形","topics":[
+{
+    "name":"三角形的分类",
+    "elementary":["538fe2a576cb8a0068b14035"],
+    "advanced":[],
+    "challenge":[]
+},{
+    "name":"三角形的三边关系",
+    "elementary":["539000fa76cb8a0068b14143"],
+    "advanced":[],
+    "challenge":["53959b4db8233b3704e37def"]
+ },{
+    "name":"三角形的高",
+    "elementary":["5390642eea8348a718c80a4b"],
+    "advanced":[],
+    "challenge":["53959b4db8233b3704e37dee"]
+ },{
+    "name":"三角形的中线与角平分线",
+    "elementary":["539174e8e66322855fba5cb0","539922c514ae70d95adf65d0"],
+    "advanced":[],
+    "challenge":["53959b4db8233b3704e37ded"]
+ },{
+    "name":"三角形的稳定性",
+    "elementary":["53918622e6fc8ec26a141077"],
+    "advanced":[],
+    "challenge":[]
+ },{
+    "name":"三角形的内角",
+    "elementary":["539297216f936dff6e86c078"],
+    "advanced":[],
+    "challenge":["5395a0d8b8233b3704e37df5"]
+ },{
+    "name":"三角形的外角",
+    "elementary":["5393d4050063d852037620f0","539ace69c9a6ce5c6c6c2c43"],
+    "advanced":[],
+    "challenge":["5395a46bb8233b3704e37dfb"]
+ },{
+    "name":"多边形",
+    "elementary":["5394119cd1474cfa17fb1bd9"],
+    "advanced":[],
+    "challenge":["53945ca944abf25b20f6363c"]
+ },{
+    "name":"多边形对角线条数",
+    "elementary":["53941efc44abf25b20f63528"],
+    "advanced":[],
+    "challenge":["5395a5c0b8233b3704e37dfd"]
+ },{
+    "name":"多边形内角和",
+    "elementary":["53944e5644abf25b20f635eb"],
+    "advanced":[],
+    "challenge":["5396613cb8233b3704e37e02"]
+ },{
+    "name":"多边形外角和",
+    "elementary":["53966359b8233b3704e37e04"],
+    "advanced":[],
+    "challenge":["53966582b8233b3704e37e24"]
+ }
+]},
+{"chapter":"全等三角形","topics":[
+{
+    "name":"全等三角形的性质",
+    "elementary":["53a107984353b42976e62dba"],
+    "advanced":[],
+    "challenge":["53a14ce94353b42976e62e31"]
+},{
+    "name":"边边边判定定理（SSS）",
+    "elementary":["53a10b654353b42976e62de3"],
+    "advanced":[],
+    "challenge":["53ad1ec6b9c8d56b7dd8a036"]
+ },{
+    "name":"边角边判定定理（SAS）",
+    "elementary":["53b129224e02f85b4d60fd0b"],
+    "advanced":[],
+    "challenge":["53aa455ba73b30a97372af30"]
+ },{
+    "name":"角边角判定定理（ASA）",
+    "elementary":["53b129264e02f85b4d60fd0c"],
+    "advanced":[],
+    "challenge":["53ad24dcb9c8d56b7dd8a058"]
+ },{
+    "name":"角角边判定定理（AAS）",
+    "elementary":["53b129284e02f85b4d60fd0d"],
+    "advanced":[],
+    "challenge":["53ad24f3b9c8d56b7dd8a059"]
+ },{
+    "name":"斜边，直角边判定定理（HL）",
+    "elementary":["53b1292c4e02f85b4d60fd0e"],
+    "advanced":[],
+    "challenge":["53aa807ea73b30a97372af5e"]
+ },{
+    "name":"角平分线的尺规作图法",
+    "elementary":["53a11b0c4353b42976e62e0e"],
+    "advanced":[],
+    "challenge":[]
+ },{
+    "name":"角平分线的性质和判定",
+    "elementary":["53b26e0cfb601327533dd007"],
+    "advanced":[],
+    "challenge":["53b0e2164e02f85b4d60fc1e"]
+ },{
+    "name":"全等模型—边",
+    "elementary":[],
+    "advanced":[],
+    "challenge":[]
+ },{
+    "name":"全等模型—角",
+    "elementary":[],
+    "advanced":[],
+    "challenge":[]
+ }
+]},
+{"chapter":"整式的乘法与因式分解","topics":[
+{
+    "name":"同底数幂的乘法",
+    "elementary":["54059542ef73b666603a6917"],
+    "advanced":[],
+    "challenge":["54059545ef73b666603a6918"]
+},{
+    "name":"幂的乘方",
+    "elementary":["54059547ef73b666603a691a"],
+    "advanced":["5421168de61af7db487ec522"],
+    "challenge":["5405954aef73b666603a691b"]
+ },{
+    "name":"积的乘方",
+    "elementary":["54059551ef73b666603a691d"],
+    "advanced":["5421169fda44c9e148cd0264"],
+    "challenge":["54211698e61af7db487ec523"]
+ },{
+    "name":"幂的运算性质总结",
+    "elementary":["540596bcef73b666603a6937"],
+    "advanced":[],
+    "challenge":["542116b4da44c9e148cd0266"]
+ },{
+    "name":"单项式乘以单项式",
+    "elementary":["540597a3ef73b666603a6949"],
+    "advanced":["5421173de61af7db487ec526"],
+    "challenge":["54211731e61af7db487ec525"]
+ },{
+    "name":"单项式乘以多项式",
+    "elementary":["54059888ef73b666603a6986"],
+    "advanced":["54211750da44c9e148cd0268"],
+    "challenge":["54211747da44c9e148cd0267"]
+ },{
+    "name":"多项式乘以多项式",
+    "elementary":["540598beef73b666603a699b"],
+    "advanced":["5421177ada44c9e148cd0269"],
+    "challenge":["54211758e61af7db487ec527"]
+ },{
+    "name":"同底数幂相除",
+    "elementary":["5417a56186d3c48052db1208"],
+    "advanced":[],
+    "challenge":["54211804da44c9e148cd026b"]
+ },{
+    "name":"单项式除以单项式",
+    "elementary":["540597b4ef73b666603a6951"],
+    "advanced":[],
+    "challenge":["5421180de61af7db487ec52c"]
+ },{
+    "name":"多项式除以单项式",
+    "elementary":["5405989aef73b666603a698b"],
+    "advanced":["5421181fda44c9e148cd026d"],
+    "challenge":["54211815e61af7db487ec52d"]
+ },{
+    "name":"平方差公式",
+    "elementary":["54059a01ef73b666603a69dd"],
+    "advanced":["5421187ae61af7db487ec532"],
+    "challenge":["5421187ae61af7db487ec532"]
+ },{
+    "name":"完全平方公式",
+    "elementary":["54059a18ef73b666603a69e3"],
+    "advanced":["54059a57ef73b666603a69f7"],
+    "challenge":["54211881da44c9e148cd0271"]
+ },{
+    "name":"用公式法因式分解",
+    "elementary":["54059ba5ef73b666603a6a1c"],
+    "advanced":["542118dbda44c9e148cd0279"],
+    "challenge":["542118dbda44c9e148cd0279"]
+ },{
+    "name":"用提公因式法因式分解",
+    "elementary":["54059beaef73b666603a6a24"],
+    "advanced":["542118e8da44c9e148cd027a"],
+    "challenge":["542118e8da44c9e148cd027a"]
+ },{
+    "name":"十字相乘法",
+    "elementary":["54059c04ef73b666603a6a29","542294eebe3a5e4d770c36b1"],
+    "advanced":[],
+    "challenge":["542118f2e61af7db487ec535"]
+ }
+]},
+{"chapter":"分式","topics":[
+{
+    "name":"分式的概念",
+    "elementary":["543e2cfac8cb2d6e1e97b6ca"],
+    "advanced":["5446015299fd66b33bd78614"],
+    "challenge":["5446015cbb5bd69a3b46167e"]
+},{
+    "name":"分式的基本性质",
+    "elementary":["544601546ac7c5b93b5b0718"],
+    "advanced":["54460157aa3a66c03b2bfb9f"],
+    "challenge":[]
+ },{
+    "name":"分式的约分与通分",
+    "elementary":["5446015a8a36fb943bc9376f","544dc34ebec68b06178382bb"],
+    "advanced":["5446016299fd66b33bd78615"],
+    "challenge":["5446015f136818a63b3a5fdb"]
+ },{
+    "name":"分式的乘除与乘方",
+    "elementary":["543e2d014dc9606c1eb3af53","54474ef7e02c67ed1a1285c7"],
+    "advanced":[],
+    "challenge":["544600e599fd66b33bd78613"]
+ },{
+    "name":"负数指数幂与科学记数法",
+    "elementary":["543e2d034dc9606c1eb3af54","5447602f2a90b9e51afbb175"],
+    "advanced":[],
+    "challenge":["54533beea3d03a99746f14b1"]
+ },{
+    "name":"分式的加减",
+    "elementary":["544600d9bb5bd69a3b46167d"],
+    "advanced":["544600dd136818a63b3a5fda"],
+    "challenge":["544600e86ac7c5b93b5b0717"]
+ },{
+    "name":"整体代入思想",
+    "elementary":[],
+    "advanced":["544600e1fd0363ac3b91065e"],
+    "challenge":["544600edaa3a66c03b2bfb9e"]
+ },{
+    "name":"分式方程",
+    "elementary":["543e2d0c4dc9606c1eb3af56"],
+    "advanced":["543e2d10c8cb2d6e1e97b6ce"],
+    "challenge":["54460011136818a63b3a5fd7"]
+ },{
+    "name":"分式方程应用题",
+    "elementary":["5446000ebb5bd69a3b46167c"],
+    "advanced":[],
+    "challenge":["54533c4fcf9c06b874bc4a17"]
+ }
+]},
+{"chapter":"二次根式","topics":[
+{
+    "name":"二次根式的定义",
+    "elementary":["5476e112202eece7320da6a1"],
+    "advanced":["5476e11746c0e3ab3248e49f"],
+    "challenge":["5476e10d820899053338b28f"]
+},{
+    "name":"二次根式的性质",
+    "elementary":["5476e114964f88c932a64b27"],
+    "advanced":["5476e119202eece7320da6a2"],
+    "challenge":["5476e10f46c0e3ab3248e49e"]
+ },{
+    "name":"二次根式的乘法",
+    "elementary":["5476e140f4978ef632cb4eef"],
+    "advanced":["5476e14446c0e3ab3248e7b8"],
+    "challenge":["5476e137e0614c9c32715ce1"]
+ },{
+    "name":"二次根式的除法",
+    "elementary":["5476e14846c0e3ab3248e7ba","5476e14be0614c9c32715ce3"],
+    "advanced":["5476e13446c0e3ab3248e74b"],
+    "challenge":["5476e13bb148d5d8329a4332"]
+ },{
+    "name":"二次根式的加减",
+    "elementary":["5476e16146c0e3ab3248e7bd"],
+    "advanced":[],
+    "challenge":["5476e1f0202eece7320da74c"]
+ }
+]},
+{"chapter":"相交线与平行线","topics":[
+{
+    "name":"对顶角和邻补角",
+    "elementary":["54927db6561e0b16499c8110","54927db7ecbb072249efd2af"],
+    "advanced":["54927da4ecbb072249efd2ae","54927db195a5763b49ab1089"],
+    "challenge":["5496cbfbae146edd74f3859f"]
+},{
+    "name":"垂线",
+    "elementary":["54927db795a5763b49ab108b"],
+    "advanced":["54927db295a5763b49ab108a","54927db3f790d0094917acf3"],
+    "challenge":[]
+ },{
+    "name":"垂线段",
+    "elementary":["54927db895a5763b49ab108c"],
+    "advanced":["54927db424325848491d12d5"],
+    "challenge":["5496cbfc0b1afbd17421c6f2"]
+ },{
+    "name":"平行线",
+    "elementary":["54927df422e3bc554907cbd3"],
+    "advanced":[],
+    "challenge":["54927dc9561e0b16499c8111"]
+ },{
+    "name":"两条直线的位置关系总结",
+    "elementary":["54927df6561e0b16499c8112"],
+    "advanced":[],
+    "challenge":[]
+ },{
+    "name":"同位角、内错角和同旁内角",
+    "elementary":["54927df9ecbb072249efd2b0","54927dfb95a5763b49ab108f"],
+    "advanced":["54927dfe95a5763b49ab1090"],
+    "challenge":["54927dea95a5763b49ab108d"]
+ },{
+    "name":"平行线的性质",
+    "elementary":["54927df124325848491d12d6"],
+    "advanced":["54927e1c561e0b16499c8113"],
+    "challenge":["54927e2c95a5763b49ab1091"]
+ },{
+    "name":"平行线的判定",
+    "elementary":["54927e329762902e494e27fe"],
+    "advanced":["54927e24ecbb072249efd2b1"],
+    "challenge":["54927e2f24325848491d12d9"]
+ },{
+    "name":"三线八角基础图总结",
+    "elementary":["54927deff790d0094917acf6"],
+    "advanced":[],
+    "challenge":[]
+ },{
+    "name":"命题、定理、证明",
+    "elementary":["54927e29f790d0094917acf7"],
+    "advanced":[],
+    "challenge":["54927e1e24325848491d12d8"]
+ },{
+    "name":"平移",
+    "elementary":["54927e34ecbb072249efd2b2"],
+    "advanced":[],
+    "challenge":["54927e219762902e494e27fd"]
+ }
+]}
+];
+
+var locateLesson = function(chapter, lessonid){
+    for (var i=0,  lola=chapter.layers.length; i < lola; i++) {
+        for (var j=0,  lole=chapter.layers[i].lessons.length; j < lole; j++) {
+            if(chapter.layers[i].lessons[j]._id == lessonid)
+                return chapter.layers[i].lessons[j];
+        }
+    }
+    return null;
+};
+
+// 容错检查，任何lessonid只许出现一次
+var lessons = [];
+
+//print('chapter total:' + chapters.length);
+for (var i=0,  loc=chapters.length; i < loc; ++i) {
+    var cursor = db.courses.find({"name":chapters[i].chapter},
+        {_id:1,name:1,description:1, layers:1});
+    if ( cursor.hasNext() ) {
+        var c = cursor.next();
+        
+        var chapter = {_id:c._id, name:c.name, 
+                          desc: c.description, topics:[] };
+        
+        for (var j=0,  lotop=chapters[i].topics.length; j < lotop; ++j) {
+            var t = chapters[i].topics[j];
+            
+            var topic = {_id:new ObjectId(), name:t.name, tasks: []};
+            chapter.topics.push(topic._id);
+            
+            for(var key in t) {
+                if(key == 'name' || t[key].length == 0) continue;
+                var task = {_id:new ObjectId(), type:key, activities:[]} 
+                
+                for (var k=0,  lotask=t[key].length; k < lotask; ++k) {
+                    var aid = t[key][k];
+                    var a = locateLesson(c, aid);
+                    if(a == null){
+                        print('error!' + aid);
+                    } else if (key == 'challenge'){
+                        if(lotask >1){
+                            print('challenge length > 1!')
+                            continue;
+                        } else {
+                            lessons.push(a._id);
+                            task._id = a._id
+                        }
+                    } else if (key == 'advanced' || key == 'elementary'){
+//                        if(lessons.indexOf(a._id)>=0){
+//                            print('redundent!' + aid);
+//                            continue;
+//                        } else {
+                            lessons.push(a._id);
+                            var activity = {_id: a._id, name: a.title}   
+                            task.activities.push(activity._id);
+                            // save activity
+                            db.activities.save(activity);
+//                            print('  ' + activity.name);   
+//                        }
+                    } else {
+                        print('type error!' + key)
+                    }
+                }
+                
+                topic.tasks.push(task._id);
+                
+                // save topic
+                db.tasks.save(task);
+//              print('  ' + task.type);
+            }
+            
+            
+            // save topic
+            db.topics.save(topic);
+//            print('  ' + topic.name);
+        }
+        
+        // save chapter
+        db.chapters.save(chapter);
+//        print(chapter.name);
+        
+    } else {
+        print('error!' + chapter[i].chapter)
+    }
+}
--- weekly/video/mostview.js
+++ weekly/video/mostview.js
@@ -1,14 +1,13 @@
 /**
  * Created by 3er on 11/24/14.
  */
-var start = ISODate("2014-10-24T00:00:00.000Z")
-var end = ISODate("2014-11-23T23:59:59.000Z")
+var start = ISODate("2014-11-10T00:00:00.000Z")
+var end = ISODate("2014-12-20T23:59:59.000Z")
 db.tracks.aggregate(
     [
-        { $match: {"data.event":"OpenVideo", "localetime":{$gt:start,$lt:end}}},
-//    { $project : { day : { $dayOfYear : "$localetime" } } } ,
-        { $group : { _id : { lessonId:"$data.properties.LessonId"} , title: {$last: "$data.properties.LessonTitle"}, visit : { $sum : 1 } } },
-        { $sort : { "visit" : -1 } }
+        { $match: {"localetime":{$gt:start,$lt:end}}},
+        { $project : { day : { $dayOfYear : "$localetime" } } } ,
+        { $group : { _id : { username:"$data.properties.username", day:"$day"} }}
     ]
 )
 
