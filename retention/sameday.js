b584b3c1dbbec94fe76bccd131833cf2be5fbe5b
stevennuo <3er.com.cn@gmail.com>
2015-03-03 00:35:08 +0800

    commit

---
+++ retention/sameday.js
@@ -0,0 +1,12 @@
+db.retention.drop();
+var start   = ISODate("2015-02-26T08:00:00.000Z")
+var end     = ISODate("2015-02-27T08:00:00.000Z")
+var next    = ISODate("2015-02-28T08:00:00.000Z")
+
+var todayA = db.points.distinct("user", {"createdBy":{$gte:start,$lt:end}});
+var today = todayA.length;
+var todayTask = db.taskstatuses.distinct("user", {"user":{$in:todayA}, "createdAt":{$gte:start,$lt:end}});
+var tmr = db.points.distinct("user", {"user":{$in:todayA}, "createdBy":{$gte:end,$lt:next}}).length;
+
+print(todayTask.length/today)
+print(tmr/today)
