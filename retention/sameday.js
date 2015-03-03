126e3ab91bbd4e0e204afb501ef5ed74178a5a35
stevennuo <3er.com.cn@gmail.com>
2015-03-03 13:25:38 +0800

    fix pass bug

--- retention/sameday.js
+++ retention/sameday.js
@@ -5,7 +5,7 @@ var next    = ISODate("2015-02-28T08:00:00.000Z")
 
 var todayA = db.points.distinct("user", {"createdBy":{$gte:start,$lt:end}});
 var today = todayA.length;
-var todayTask = db.taskstatuses.distinct("user", {"user":{$in:todayA}, "createdAt":{$gte:start,$lt:end}});
+var todayTask = db.taskstatuses.distinct("user", {"user":{$in:todayA}, isPassed:true,  "createdAt":{$gte:start,$lt:end}});
 var tmr = db.points.distinct("user", {"user":{$in:todayA}, "createdBy":{$gte:end,$lt:next}}).length;
 
 print(todayTask.length/today)
