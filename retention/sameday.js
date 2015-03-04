4833bed71bcd2cd0aa23522c7c67dfe7f3149718
stevennuo <3er.com.cn@gmail.com>
2015-03-04 12:58:20 +0800

    update retention script

--- retention/sameday.js
+++ retention/sameday.js
@@ -1,12 +1,14 @@
 db.retention.drop();
-var start   = ISODate("2015-02-26T08:00:00.000Z")
-var end     = ISODate("2015-02-27T08:00:00.000Z")
-var next    = ISODate("2015-02-28T08:00:00.000Z")
+var start   = ISODate("2015-02-13T00:00:00+08:00")
+var end     = ISODate("2015-02-14T00:00:00+08:00")
+var next    = ISODate("2015-02-15T00:00:00+08:00")
 
-var todayA = db.points.distinct("user", {"createdBy":{$gte:start,$lt:end}});
+var todayA = db.users.distinct("_id", {"usefulData.registDate":{$gte:start,$lt:end}});
 var today = todayA.length;
 var todayTask = db.taskstatuses.distinct("user", {"user":{$in:todayA}, isPassed:true,  "createdAt":{$gte:start,$lt:end}});
 var tmr = db.points.distinct("user", {"user":{$in:todayA}, "createdBy":{$gte:end,$lt:next}}).length;
 
+print(today)
+print(todayTask.length)
 print(todayTask.length/today)
 print(tmr/today)
